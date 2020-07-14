fe17136aa7d2f32999df38e66d40b05073b3b267
Rasmus Diamant <razbook3.0@Rasmuss-MacBook-Pro.local>
2020-07-14 22:58:57 +0200

    Added test background

--- Assets/Scripts/AudioScripts/ToggleBackgroundMusic.cs
+++ Assets/Scripts/AudioScripts/ToggleBackgroundMusic.cs
@@ -8,27 +8,26 @@ using UnityEngine.UI;
 public class ToggleBackgroundMusic : MonoBehaviour {
     public AudioMixer masterMixer;
     public string parameterName = "MasterVolume";
+    private bool justMuted = false;
     
-    
-
     private void Start () {
         Globals.prevMasterVol = GetBackgroundLevel ();
     }
+
     public void ToggleMusic () {
-        Debug.Log ("VOL");
         float volume = GetBackgroundLevel ();
 
         if (volume != Globals.minVol) {
-            masterMixer.SetFloat (parameterName, Globals.minVol); //BackgroundMusic
+            justMuted = true;
+            SetMainMixVolume(Globals.minVol); //BackgroundMusic
         } else {
-            masterMixer.SetFloat (parameterName, Globals.prevMasterVol);
+            SetMainMixVolume(Globals.prevMasterVol);
         }
     }
 
     private float GetBackgroundLevel () {
         float value;
         bool result = masterMixer.GetFloat (parameterName, out value);
-        Debug.Log ("Result: " + result + "Value: " + value);
         if (result) {
         return value;
         } else {
@@ -37,13 +36,21 @@ public class ToggleBackgroundMusic : MonoBehaviour {
     }
 
     public void SetSound (float soundLevel) {
+        if (justMuted){
+            justMuted = false;
+        }else{
         var actualSoundLevel = calculatedB(soundLevel);
-        masterMixer.SetFloat (parameterName, actualSoundLevel);
-        Globals.prevMasterVol = soundLevel;
+        SetMainMixVolume(actualSoundLevel);
+        Globals.prevMasterVol = actualSoundLevel;
+        }
     }
 
+    private void SetMainMixVolume(float val){
+        masterMixer.SetFloat (parameterName, val);
+    }
     // Max volume is +20 but we limit to 0, otherwise we will introduce distorsion
     private float calculatedB (float val) {
+        Debug.Log("Val: " + val + "    LogVal: " + (Mathf.Log10(val) * 20));
         float normalizedValue = Mathf.InverseLerp (0f, 1f, val); // Input from Slider 0 - 1
         return Mathf.Lerp (Globals.minVol, 0f, normalizedValue); // Convert linear to dB
     }
